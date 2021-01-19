bcfe868f43bdff90e00e1f96986c3f7ba61a1f83
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2021-01-19 10:30:34 +0000

    Fixing integration tests

--- app/initializer/initializer.go
+++ app/initializer/initializer.go
@@ -13,4 +13,8 @@ import (
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/too
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+const (
+	defaultIni
