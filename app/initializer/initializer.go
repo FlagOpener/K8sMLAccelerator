b3656f5ac68a3b8bfedc62cf904072b9009eba74
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2021-02-02 18:04:55 +0000

    Setting version to 0.9.1-SNAPSHOT

--- app/initializer/initializer.go
+++ app/initializer/initializer.go
@@ -30,4 +30,4 @@ var (
 func main() {
 	flag.StringVar(&controller.IntializerConfigmapName, "configmap", defaultConfigmapName, "initializer configuration configmap")
 	flag.StringVar(&controller.InitializerName, "initializer-name", defaultInitializerName, "The initializer name")
-	flag.StringVar(&controller.IntializerNamespace, "namespace", default
+	flag.StringVar(&controller.IntializerNamespace, "namespace", defaultConfigMapNamespace, "The conf
