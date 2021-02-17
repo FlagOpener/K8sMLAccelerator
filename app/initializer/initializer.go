82992662e777ce097737181bfc7c584106267fb1
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2021-02-17 07:43:00 +0000

    delete some files

--- app/initializer/initializer.go
+++ app/initializer/initializer.go
@@ -31,4 +31,5 @@ func main() {
 	flag.StringVar(&controller.IntializerConfigmapName, "configmap", defaultConfigmapName, "initializer configuration configmap")
 	flag.StringVar(&controller.InitializerName, "initializer-name", defaultInitializerName, "The initializer name")
 	flag.StringVar(&controller.IntializerNamespace, "namespace", defaultConfigMapNamespace, "The configuration namespace")
-	flag.StringVar(&kubeConfig, "kubeconfig", "", "Absolute path to
+	flag.StringVar(&kubeConfig, "kubeconfig", "", "Absolute path to the kubeconfig")
+	flag.S
