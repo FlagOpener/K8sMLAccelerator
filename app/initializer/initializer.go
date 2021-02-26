e47f8a5b5d6f15c5f0c226242d4292d5acb46c08
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2021-02-26 12:08:29 +0000

    6.0

--- app/initializer/initializer.go
+++ app/initializer/initializer.go
@@ -41,3 +41,4 @@ func main() {
 	if len(kubeMaster) > 0 || len(kubeConfig) > 0 {
 		clusterConfig, err = clientcmd.BuildConfigFromFlags(kubeMaster, kubeConfig)
 	} else {
+		clusterConfig, err = rest.InClus
