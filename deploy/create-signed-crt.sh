5d0135577d879c51ac3ffeb5e652fe8281aa37e3
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2021-03-31 15:11:21 +0000

    remove printf

--- deploy/create-signed-crt.sh
+++ deploy/create-signed-crt.sh
@@ -5,4 +5,4 @@ set -e
 usage() {
     cat <<EOF
 Generate certificate suitable for use with an hostaliases-injector webhook service.
-This script uses k8s'
+This script uses k8s' CertificateSigningRequest API t
