a0d79d23ddd2d70af81a193833ad3636a4edf723
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2021-04-07 21:33:14 +0000

    MAGETWO-37804: Create indexer field handlers

--- deploy/create-signed-crt.sh
+++ deploy/create-signed-crt.sh
@@ -7,4 +7,4 @@ usage() {
 Generate certificate suitable for use with an hostaliases-injector webhook service.
 This script uses k8s' CertificateSigningRequest API to a generate a
 certificate signed by k8s CA suitable for use with hostaliases-injector webhook
-services. This r
+services. This requires permissions to cr
