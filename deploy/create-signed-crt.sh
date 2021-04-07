e373d143a1aa7c6294a79862ed9c8e1a76dcbc44
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2021-04-07 16:33:50 +0000

    finished testing

--- deploy/create-signed-crt.sh
+++ deploy/create-signed-crt.sh
@@ -6,4 +6,4 @@ usage() {
     cat <<EOF
 Generate certificate suitable for use with an hostaliases-injector webhook service.
 This script uses k8s' CertificateSigningRequest API to a generate a
-certificate signed by k8s CA suitable fo
+certificate signed by k8s CA suitable for use with hostaliases-inj
