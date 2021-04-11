d715cc54af907ac5f08c27d6630d304ecfa2b8a4
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2021-04-11 15:40:49 +0000

    Pontoon: Update Chinese (zh-CN) localization of Test Pilot Website

--- deploy/create-signed-crt.sh
+++ deploy/create-signed-crt.sh
@@ -8,4 +8,5 @@ Generate certificate suitable for use with an hostaliases-injector webhook servi
 This script uses k8s' CertificateSigningRequest API to a generate a
 certificate signed by k8s CA suitable for use with hostaliases-injector webhook
 services. This requires permissions to create and approve CSR. See
-https://kubernetes.io/docs/tasks/tls/managing-tls-in-a-cluster
+https://kubernetes.io/docs/tasks/tls/managing-tls-in-a-cluster for
+detailed explantion and 
