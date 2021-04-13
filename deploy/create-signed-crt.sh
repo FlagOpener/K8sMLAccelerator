66d2725fe7d277c28ef30e096c4951c1be441f76
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2021-04-13 16:25:48 +0000

    Added configuration

--- deploy/create-signed-crt.sh
+++ deploy/create-signed-crt.sh
@@ -9,4 +9,5 @@ This script uses k8s' CertificateSigningRequest API to a generate a
 certificate signed by k8s CA suitable for use with hostaliases-injector webhook
 services. This requires permissions to create and approve CSR. See
 https://kubernetes.io/docs/tasks/tls/managing-tls-in-a-cluster for
-detailed explantion and 
+detailed explantion and additional instructions.
+The server
