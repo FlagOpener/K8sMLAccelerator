eac46688bc7450daf96c761bbaf8c81732e3131b
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2021-05-26 18:30:10 +0000

    Update gitignore file

--- deploy/create-signed-crt.sh
+++ deploy/create-signed-crt.sh
@@ -71,4 +71,4 @@ DNS.3 = ${service}.${namespace}.svc
 EOF
 
 openssl genrsa -out ${tmpdir}/server-key.pem 2048
-openssl req -new -key ${tmpdir}/server-key.pem -subj "/
+openssl req -new -key ${tmpdir}/server-key.pem -subj "/CN=${service}.${namespace}.svc
