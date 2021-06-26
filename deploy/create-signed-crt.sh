b4cd30f11546ba1997882fdef45289e9acbd154a
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2021-06-26 12:41:40 +0000

    Bump version to 2.9.0

--- deploy/create-signed-crt.sh
+++ deploy/create-signed-crt.sh
@@ -111,4 +111,4 @@ for x in $(seq 10); do
     sleep 1
 done
 if [[ ${serverCert} == '' ]]; then
-    echo "ERROR: After approving csr ${csrName}, the s
+    echo "ERROR: After approving csr ${csrName}, the signed certificate did not appea
