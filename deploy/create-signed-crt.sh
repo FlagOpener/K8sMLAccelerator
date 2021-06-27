274c66bc7a9b54ad9fe9376b4d02f6e89bbbfc78
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2021-06-27 12:32:22 +0000

    update commands

--- deploy/create-signed-crt.sh
+++ deploy/create-signed-crt.sh
@@ -111,4 +111,4 @@ for x in $(seq 10); do
     sleep 1
 done
 if [[ ${serverCert} == '' ]]; then
-    echo "ERROR: After approving csr ${csrName}, the signed certificate did not appear on the resource. Givin
+    echo "ERROR: After approving csr ${csrName}, the signed certificate did not appear on the resource. Giving up after 10 attempts.
