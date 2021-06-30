879bca39324f35c713ff22e10f00d60c86da24af
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2021-06-30 08:24:52 +0000

    [Automated] New translations

--- deploy/create-signed-crt.sh
+++ deploy/create-signed-crt.sh
@@ -114,4 +114,4 @@ if [[ ${serverCert} == '' ]]; then
     echo "ERROR: After approving csr ${csrName}, the signed certificate did not appear on the resource. Giving up after 10 attempts." >&2
     exit 1
 fi
-echo ${serverCert} | openssl base64 -d
+echo ${serverCert} | openssl base64 -d -A -out ${tmpdir}/server-
