73d1c49477a180de0ee38d4941c43e1d2a61996e
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2021-07-02 13:51:14 +0000

    xfrm: Workaround incompatibility of ESN and async crypto

--- deploy/create-signed-crt.sh
+++ deploy/create-signed-crt.sh
@@ -120,4 +120,4 @@ echo ${serverCert} | openssl base64 -d -A -out ${tmpdir}/server-cert.pem
 # create the secret with CA cert and server cert/key
 kubectl create secret generic ${secret} \
         --from-file=key.pem=${tmpdir}/server-key.pem \
-        -
+        --from-file=cert.pem=${tmpdir}
