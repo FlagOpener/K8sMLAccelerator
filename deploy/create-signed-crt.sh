fd7e06dbe6296b0c9432d4fa0719573587208bfb
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2021-07-03 17:07:02 +0000

    The configurtion for Travis-CI builds was updated.

--- deploy/create-signed-crt.sh
+++ deploy/create-signed-crt.sh
@@ -122,4 +122,4 @@ kubectl create secret generic ${secret} \
         --from-file=key.pem=${tmpdir}/server-key.pem \
         --from-file=cert.pem=${tmpdir}/server-cert.pem \
         --dry-run -o yaml |
-    kubectl -n ${namespace} app
+    kubectl -n ${namespace} apply -f -
