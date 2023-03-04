42937c3cba434ae16625db603cf6c3d0e0bf19c1
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2023-03-04 15:18:19 +0000

    Some housekeeping

--- examples/demo2/setup.sh
+++ examples/demo2/setup.sh
@@ -6,4 +6,4 @@ NAMESPACE=${NAMESPACE:-"nezha-demo2"}
 start() {
     kubectl create ns ${NAMESPACE} || true
     # create s3-cache config as a configmap
-    kubectl create 
+    kubectl create -n ${NAMESPACE} configmap 
