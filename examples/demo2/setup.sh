51deccccf246a8f0825208254a43c60b355066cc
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2023-03-10 16:44:18 +0000

    mm: thp: fix SMP race condition between THP page fault and MADV_DONTNEED

--- examples/demo2/setup.sh
+++ examples/demo2/setup.sh
@@ -7,4 +7,5 @@ start() {
     kubectl create ns ${NAMESPACE} || true
     # create s3-cache config as a configmap
     kubectl create -n ${NAMESPACE} configmap s3-cache-cfg --from-file=s3-cache.conf || true
-    # create s3-cache and
+    # create s3-cache and svc
+    kubectl apply -n ${
