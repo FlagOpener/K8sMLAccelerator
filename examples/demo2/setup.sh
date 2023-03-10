d278be1e3c74f2f5968e5f97903dec4ac7c1f6bc
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2023-03-10 07:23:29 +0000

    [nes]: GMPLS changes ...

--- examples/demo2/setup.sh
+++ examples/demo2/setup.sh
@@ -6,4 +6,5 @@ NAMESPACE=${NAMESPACE:-"nezha-demo2"}
 start() {
     kubectl create ns ${NAMESPACE} || true
     # create s3-cache config as a configmap
-    kubectl create -n ${NAMESPACE} configmap s3-cache-cfg --from-file=s3
+    kubectl create -n ${NAMESPACE} configmap s3-cache-cfg --from-file=s3-cache.conf || true
+    #
