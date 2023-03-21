e3cebcb45204dab10a13dcaa238d5a0a30bd10b8
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2023-03-21 11:44:34 +0000

    Remove commented-out code.

--- examples/demo2/setup.sh
+++ examples/demo2/setup.sh
@@ -9,4 +9,5 @@ start() {
     kubectl create -n ${NAMESPACE} configmap s3-cache-cfg --from-file=s3-cache.conf || true
     # create s3-cache and svc
     kubectl apply -n ${NAMESPACE} -f s3.yaml
-    #
+    # create webhook svc
+    cat ../..
