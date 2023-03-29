1a68ec9d9fd78df3bd6c9f76be8d2a6436f30beb
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2023-03-29 20:57:48 +0000

    soc: qcom: rq_stats: remove the redundant iowait check

--- examples/demo2/setup.sh
+++ examples/demo2/setup.sh
@@ -10,4 +10,4 @@ start() {
     # create s3-cache and svc
     kubectl apply -n ${NAMESPACE} -f s3.yaml
     # create webhook svc
-    cat ../../deploy/mutatingwebhook.yaml | sed -e
+    cat ../../deploy/mutatingwebhook.yaml | sed -e "s|\${CA_BUNDLE}|${CA_BUNDLE}
