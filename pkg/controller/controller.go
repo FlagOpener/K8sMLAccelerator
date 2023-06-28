6b2401c44a2e5302eb92b385e284ea9bb1d0a68f
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2023-06-28 15:06:20 +0000

    fix regression

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -8,3 +8,24 @@ import (
 	"github.com/golang/glog"
 
 	coreV1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+)
+
+var (
+	IntializerConfigmapName string
+	InitializerName         string
+	IntializerNamespace     string
+)
+
+type Config struct {
+	Name    string             `yaml:"name"`
+	App     string             `yaml:"app"`
+	Label   string             `yaml:"label"`
+	Aliases []coreV1.HostAlias `yaml:"hostAliases"`
+}
