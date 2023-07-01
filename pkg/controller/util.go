f1f8d5d3bbd2472099b4577274a8e9360916896d
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2023-07-01 19:11:27 +0000

    Avances

--- pkg/controller/util.go
+++ pkg/controller/util.go
@@ -26,3 +26,30 @@ func GetAliases(app string, config []Config) []coreV1.HostAlias {
 func GetAliasesByKV(k, v string, config []Config) []coreV1.HostAlias {
 	for _, conf := range config {
 		glog.V(5).Infof("looking for %s, %s using %s", k, v, conf.Label)
+		if conf.App == k && conf.Label == v {
+			return conf.Aliases
+		}
+	}
+	return nil
+}
+
+func ConfigMapToConfig(cm *coreV1.ConfigMap) (*[]Config, error) {
+	var c []Config
+	err := yaml.Unmarshal([]byte(cm.Data["config"]), &c)
+	if err != nil {
+		return nil, err
+	}
+	glog.V(5).Infof("configs %+v", c)
+	return &c, err
+}
+
+func FileToConfig(filePath string) (*[]Config, error) {
+	var c []Config
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open %s: %v", filePath, err)
+	}
+	err = yaml.Unmarshal(data, &c)
+	if err != nil {
+		return nil, err
+	}
